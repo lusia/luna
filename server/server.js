42de912657ad7cfe5de4ddeaabf61cb76ff664f6
Elzbieta Leska-Twarog <elaleska@gmail.com>
2014-05-11 13:54:52 +0200

    Created server.js where publish function was added and collections.js to declare a collection

---
+++ server/server.js
@@ -0,0 +1,35 @@
+Meteor.startup(function () {
+
+	// code to run on server at startup
+	if (Meals.find().count() == 0) {
+		var meals = [
+			{
+				"name": "meal 1",
+				"price": 30,
+				"category": "a",
+				"calories": 150
+			},
+			{
+				"name": "meal 3",
+				"price": 30,
+				"category": "b",
+				"calories": 150
+			},
+			{
+				"name": "meal 4",
+				"price": 30,
+				"category": "b",
+				"calories": 150
+			}
+		];
+
+		_.each(meals, function (doc) {
+			Meals.insert(doc);
+		});
+	}
+
+});
+
+Meteor.publish("meals", function () {
+	return Meals.find();
+});
